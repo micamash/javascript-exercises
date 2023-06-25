665b1fc187733b18c9be0ad4e63ff1abac111edd
Ashley Mical <ashleymmical@gmail.com>
2023-06-25 11:22:15 -0500

    completed 1-3 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    const stringArray = string.split('');
+    const reversedArray = stringArray.reverse();
+    const reversedString = reversedArray.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
